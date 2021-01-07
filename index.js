3408c110a1fa2810bd9cf7652ed1fc3b61c75fbb
Edgar <egmez124@gmail.com>
2021-01-07 17:10:37 -0600

    con mime

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 let http = require("http");
 let fs = require("fs");
+let path = require("path");
+let mime = require("mime")
 
 http.createServer((request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     switch(request.url) {
         case "/":
-            readFile("./index.html", response)
+            readFile("/index.html", response)
         break;
         case "/contacto":
-            readFile("./contact.html", response)
+            readFile("/contact.html", response)
         break;
         case "/nosotros":
-            readFile("./about.html", response)
+            readFile("/about.html", response)
         break;
         case "/proyectos":
-            readFile("./projects.html", response)
+            readFile("/projects.html", response)
         break;
         case "/favicon.ico":
-            readFile("./favicon.ico", response)
+            readFile("/favicon.ico", response)
         break;
         default:
-            readFile("./404.html", response)
+            readFile(request.url, response)
         break;
     }
 }).listen(80);
 
-const readFile = (path, response) => {
-    fs.readFile (path, (error, content) => {
+const readFile = (url, response) => {
+    let urlF = __dirname + url
+    fs.readFile (urlF, (error, content) => {
         if(!error){
-            response.write(content);
-            response.end();
+            // setContentType(path.extname(urlF), response)
+            //mime.getType -> Regresa el mime type dependiendo de la ruta
+            //styles.css -> text/css
+            //index.html -> text/html
+            response.setHeader("Content-Type", mime.getType(urlF))
+            response.end(content);
+        }else{
+            response.writeHead(404);
+            response.end("<h1>Error 404</h1>");
         }
     });
 }
