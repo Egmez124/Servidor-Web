a9174ba38ecadaf52ad7adf2feb06b3abe80b52f
Edgar <egmez124@gmail.com>
2021-01-08 17:02:36 -0600

    Evalucion node semana 1

--- index.js
+++ index.js
@@ -1,92 +1,42 @@
-let http = require("http");
+let express = require("express");
 let fs = require("fs");
 let path = require("path");
-let mime = require("mime")
+let app = express();
 
-http.createServer((request, response) => {
-    // response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if(request.method === "GET") {
-        switch(request.url) {
-            case "/":
-                readFile("/index.html", response)
-            break;
-            case "/contacto":
-                readFile("/contact.html", response)
-            break;
-            case "/nosotros":
-                readFile("/about.html", response)
-            break;
-            case "/proyectos":
-                readFile("/projects.html", response)
-            break;
-            case "/registro":
-                readFile("/registro.html", response)
-            break;
-            case "/favicon.ico":
-                readFile("/favicon.ico", response)
-            break;
-            default:
-                readFile(request.url, response)
-            break;
-        }
-    }else if(request.method === "POST"){
-        switch(request.url) {
-            case "/users":
-                addUser(request);
-            break;
-            default:
-                readFile(request.url, response)
-            break;
-        }
-    }
-}).listen(80);
+//Middlewares
+//accede a css que esta en la carpeta public 
+app.use(express.static("public"));
+//procesa los datos que son enviados a traves del formulario
+app.use(express.urlencoded({extended: false})); 
 
-const readFile = (url, response) => {
-    let urlF = __dirname + url
-    fs.readFile (urlF, (error, content) => {
-        if(!error){
-            // setContentType(path.extname(urlF), response)
-            //mime.getType -> Regresa el mime type dependiendo de la ruta
-            //styles.css -> text/css
-            //index.html -> text/html
-            response.setHeader("Content-Type", mime.getType(urlF))
-            response.end(content);
-        }else{
-            response.writeHead(404);
-            response.end("<h1>Error 404</h1>");
+app.get("/", (request, response)=> {
+    response.sendFile(path.join(__dirname, "index.html"));
+ });
+
+app.get("/contacto", (request, response)=> {
+    response.sendFile(path.join(__dirname, "contact.html"));
+ });
+app.get("/nosotros", (request, response)=> {
+    response.sendFile(path.join(__dirname, "about.html"));
+ });
+app.get("/contacto", (request, response)=> {
+    response.sendFile(path.join(__dirname, "contact.html"));
+ });
+
+app.get("/registro", (request, response)=> {
+    response.sendFile(path.join(__dirname, "registro.html"));
+ });
+app.use( (request, response)=> {
+    response.sendFile(path.join(__dirname, "404.html"));
+ });
+app.post("/usuarios", ( request, response) => {
+    fs.writeFile("db_usuarios.txt", JSON.stringify(request.body), (error)=>{
+        if(error){
+            console.log(error);
         }
     });
-}
+});
+app.listen(8080, () => {
+    console.log("Servidor 8080")
+});
 
-const addUser = (request)=>{
-    let data = '';
-        //Cuando se estan escribiendo los datos
-        request.on('data', chunk => {
-            data += chunk;
-        });
-        //Cuando se terminen de procesar los datos
-        request.on('end', () => {
-            let datos = data.toString();
-            console.log("Fin de Stream");
-            //writeFile
-            //1er argumento -> La ruta del archivo en el que queremos escribir
-            //Se creará el erchivo si no existe en la ruta especificada
-            //2do argumento -> El contenido que queremos escribir
-            //3er argumento -> funcion de callback que nos notificará en caso que exista un error 
-            //al escribir el archivo
-            let user = {
-                name: datos.split("&")[0].split("=")[1],
-                lastname: datos.split("&")[1].split("=")[1],
-                email: datos.split("&")[2].split("=")[1],
-                password: datos.split("&")[3].split("=")[1]
-            }
-            fs.writeFile("db_usuarios.txt", JSON.stringify(user), (error)=>{
-                if(error){
-                    console.log(error);
-                }
-            });
-        });
-        request.on('error',error =>{
-            console.log(error);
-        });
-}
