ebd3b7653a00f871c2aa4f8c50b9bc9e8e5f1eed
Edgar <egmez124@gmail.com>
2021-01-08 15:27:11 -0600

    agregando formulario y POST en el servidor

--- index.js
+++ index.js
@@ -4,26 +4,40 @@ let path = require("path");
 let mime = require("mime")
 
 http.createServer((request, response) => {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
-    switch(request.url) {
-        case "/":
-            readFile("/index.html", response)
-        break;
-        case "/contacto":
-            readFile("/contact.html", response)
-        break;
-        case "/nosotros":
-            readFile("/about.html", response)
-        break;
-        case "/proyectos":
-            readFile("/projects.html", response)
-        break;
-        case "/favicon.ico":
-            readFile("/favicon.ico", response)
-        break;
-        default:
-            readFile(request.url, response)
-        break;
+    // response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if(request.method === "GET") {
+        switch(request.url) {
+            case "/":
+                readFile("/index.html", response)
+            break;
+            case "/contacto":
+                readFile("/contact.html", response)
+            break;
+            case "/nosotros":
+                readFile("/about.html", response)
+            break;
+            case "/proyectos":
+                readFile("/projects.html", response)
+            break;
+            case "/registro":
+                readFile("/registro.html", response)
+            break;
+            case "/favicon.ico":
+                readFile("/favicon.ico", response)
+            break;
+            default:
+                readFile(request.url, response)
+            break;
+        }
+    }else if(request.method === "POST"){
+        switch(request.url) {
+            case "/users":
+                addUser(request);
+            break;
+            default:
+                readFile(request.url, response)
+            break;
+        }
     }
 }).listen(80);
 
@@ -42,4 +56,37 @@ const readFile = (url, response) => {
             response.end("<h1>Error 404</h1>");
         }
     });
+}
+
+const addUser = (request)=>{
+    let data = '';
+        //Cuando se estan escribiendo los datos
+        request.on('data', chunk => {
+            data += chunk;
+        });
+        //Cuando se terminen de procesar los datos
+        request.on('end', () => {
+            let datos = data.toString();
+            console.log("Fin de Stream");
+            //writeFile
+            //1er argumento -> La ruta del archivo en el que queremos escribir
+            //Se creará el erchivo si no existe en la ruta especificada
+            //2do argumento -> El contenido que queremos escribir
+            //3er argumento -> funcion de callback que nos notificará en caso que exista un error 
+            //al escribir el archivo
+            let user = {
+                name: datos.split("&")[0].split("=")[1],
+                lastname: datos.split("&")[1].split("=")[1],
+                email: datos.split("&")[2].split("=")[1],
+                password: datos.split("&")[3].split("=")[1]
+            }
+            fs.writeFile("db_usuarios.txt", JSON.stringify(user), (error)=>{
+                if(error){
+                    console.log(error);
+                }
+            });
+        });
+        request.on('error',error =>{
+            console.log(error);
+        });
 }
