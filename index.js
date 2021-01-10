22d7ef0188f14fa26e2f55d671e2ddc516fc7452
Edgar <johndoe@example.com>
2021-01-10 14:10:34 -0600

    Evaluación node

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ let app = express();
 //Middlewares
 //accede a css que esta en la carpeta public 
 app.use(express.static("public"));
-//procesa los datos que son enviados a traves del formulario
+//procesa los datos que son enviados a traves del formulario 
+//Va a colocar los datos que se transformaron sobre request.body
 app.use(express.urlencoded({extended: false})); 
 
 app.get("/", (request, response)=> {
@@ -25,17 +26,21 @@ app.get("/contacto", (request, response)=> {
 
 app.get("/registro", (request, response)=> {
     response.sendFile(path.join(__dirname, "registro.html"));
- });
-app.use( (request, response)=> {
-    response.sendFile(path.join(__dirname, "404.html"));
- });
+ }); 
+
 app.post("/usuarios", ( request, response) => {
     fs.writeFile("db_usuarios.txt", JSON.stringify(request.body), (error)=>{
         if(error){
             console.log(error);
         }
+        //Después que se escribe el archivo txt redirecciona a la ruta que deseemos.
+        response.redirect("/");
     });
 });
+app.use( (request, response)=> {
+    response.sendFile(path.join(__dirname, "404.html"));
+ });
+
 app.listen(8080, () => {
     console.log("Servidor 8080")
 });
